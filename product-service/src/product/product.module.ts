cd7cb247ced16c124e367dc5ef440d72c7a73115
Lordxan <52027483+Lordxan@users.noreply.github.com>
2022-07-24 22:33:44 +0400

    Added models & rds resource

--- product-service/src/product/product.module.ts
+++ product-service/src/product/product.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 import { ProductController } from './product.controller';
-import { ProductService } from './product.service';
 
 @Module({
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [PrismaService],
 })
 export class ProductModule {}
