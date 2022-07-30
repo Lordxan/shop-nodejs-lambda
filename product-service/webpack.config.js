abd3281502a51c2888c1205effdc0fb3434cfaff
Lordxan <52027483+Lordxan@users.noreply.github.com>
2022-07-30 19:19:21 +0400

    Updated webpack config

--- product-service/webpack.config.js
+++ product-service/webpack.config.js
@@ -1,9 +1,35 @@
-module.exports = (options) => {
+/* eslint-disable */
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = (options, webpack) => {
+  const lazyImports = [
+    '@nestjs/microservices/microservices-module',
+    '@nestjs/websockets/socket-module',
+    'class-transformer',
+    'class-transformer/storage',
+    '@prisma/client',
+  ];
+
   return {
     ...options,
-    output: {
-      ...options.output,
-      libraryTarget: 'commonjs2',
-    },
+    externals: [],
+    plugins: [
+      ...options.plugins,
+      new webpack.IgnorePlugin({
+        checkResource(resource) {
+          if (lazyImports.includes(resource)) {
+            try {
+              require.resolve(resource);
+            } catch (err) {
+              return true;
+            }
+          }
+          return false;
+        },
+      }),
+      new CopyPlugin({
+        patterns: ['./prisma/schema.prisma', './.env'],
+      }),
+    ],
   };
 };
